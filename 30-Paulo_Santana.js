4c856b7dd54b914cdfa5d5894550731a2c9c6b50
Paulo Abreu <micaeler00@gmail.com>
2022-02-16 09:59:15 -0300

    feat: remove car button

--- 30-Paulo_Santana.js
+++ 30-Paulo_Santana.js
@@ -12,17 +12,19 @@
   let $companyName = new DOM('[data-js="logo-name"]').get()[0];
   let $companyPhone = new DOM('[data-js="phone"]').get()[0];
 
+  let carID = 0;
+  let cardId = 0;
   let cars = [];
 
   function handleSubmit(e) {
     e.preventDefault();
-    let fieldsEmpty = validateFields();
 
-    if (fieldsEmpty) {
+    if (validateFields()) {
       return alert("Por favor preencher todos os campos");
     }
 
     cars.push({
+      id: (carID += 1),
       img: $imgInput.value,
       brand: $brandInput.value,
       model: $modelInput.value,
@@ -30,25 +32,39 @@
       board: $boardInput.value,
       color: $colorInput.value,
     });
-    populateHTML();
 
+    populateHTML();
     $form.get()[0].reset();
   }
 
-  $form.on("submit", handleSubmit);
+  function removeCarHandler(event) {
+    let element = event.target.parentElement;
+
+    $catalogContainer.removeChild(element);
+
+    let carForRemove = cars.filter((car) => car.id === Number(element.id));
+
+    cars.pop(carForRemove);
+  }
 
   function populateHTML() {
-    let cardCatalog = document.createElement("div");
+    let $cardCatalog = document.createElement("div");
+    let $deleteCardButton = document.createElement("button");
+
+    $deleteCardButton.innerText = "Excluir";
+
+    $cardCatalog.id = cardId += 1;
+    $deleteCardButton.addEventListener("click", removeCarHandler);
 
     cars.forEach((car) => {
       let HTMLTemplate = `
-          <div class="box-image">
-            <img
-              src="${car.img}"
-              alt=""
-              class="car-image"
-            >
-          </div>
+        <div class="box-image">
+          <img
+            src="${car.img}"
+            alt=""
+            class="car-image"
+          >
+        </div>
         <div class="box-info">
           <div class="name-year">
             <span>${car.brand}: ${car.model}</span>
@@ -58,11 +74,14 @@
             <span>Placa: ${car.board}</span>
             <span>Cor: ${car.color}</span>
           </div>
-        </div>`;
+        </div>
+        `;
 
-      cardCatalog.classList.add("card");
-      cardCatalog.innerHTML = HTMLTemplate;
-      return $catalogContainer.appendChild(cardCatalog);
+      $cardCatalog.classList.add("card");
+      $cardCatalog.innerHTML = HTMLTemplate;
+      $cardCatalog.appendChild($deleteCardButton);
+
+      return $catalogContainer.appendChild($cardCatalog);
     });
   }
 
@@ -109,31 +128,5 @@
     $companyPhone.innerText = dataJson.phone;
   });
 
-  /*
-A loja de carros será nosso desafio final. Na aula anterior, você fez a parte
-do cadastro dos carros. Agora nós vamos começar a deixar ele com cara de
-projeto mesmo.
-Crie um novo repositório na sua conta do GitHub, com o nome do seu projeto.
-Na hora de criar, o GitHub te dá a opção de criar o repositório com um
-README. Use essa opção.
-Após criar o repositório, clone ele na sua máquina.
-Crie uma nova branch chamada `challenge-30`, e copie tudo o que foi feito no
-desafio da aula anterior para esse novo repositório, nessa branch
-`challenge-30`.
-Adicione um arquivo na raiz desse novo repositório chamado `.gitignore`.
-O conteúdeo desse arquivo deve ser somente as duas linhas abaixo:
-node_modules
-npm-debug.log
-Faça as melhorias que você achar que são necessárias no seu código, removendo
-duplicações, deixando-o o mais legível possível, e então suba essa alteração
-para o repositório do seu projeto.
-Envie um pull request da branch `challenge-30` para a `master` e cole aqui
-nesse arquivo, dentro do `console.log`, o link para o pull request no seu
-projeto.
-*/
-
-  console.log("Link do pull request do seu projeto:");
-  console.log(
-    "https://github.com/paulozy/AutoLuby-NinjaJS/commit/9a2cd5d694e01717975b8d836da633c61bc85080"
-  );
+  $form.on("submit", handleSubmit);
 })(window.DOM);
